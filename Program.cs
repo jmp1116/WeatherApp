67052aea1b9a1247f8765a1f8593a9809a8b30f6
John Mark <jmp1116@gmail.com>
2020-11-30 20:37:27 -0600

    got temp

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace WeatherMap
 {
@@ -6,7 +9,17 @@ namespace WeatherMap
     {
         static void Main(string[] args)
         {
-            
+            string key = File.ReadAllText("appsettings.json");
+            string apiKey = JObject.Parse(key).GetValue("DefaultKey").ToString();
+
+            Console.WriteLine("Please enter your zip code");
+            string zipCode = Console.ReadLine();
+            Console.WriteLine("Please enter your country code");
+            string countryCode = Console.ReadLine().ToLower();
+
+            string apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},{countryCode}&appid={apiKey}";
+
+            Console.WriteLine(WeatherMap.GetTemp(apiCall));
         }
     }
 }
